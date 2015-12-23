8734a3e612e303a65c4f33844193f193135ad67d
dis-zephyro <zephyro@yandex.ru>
2015-12-23 17:39:39 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ $('.cert ul').slick({
     slidesToScroll: 1,
     responsive: [
         {
-            breakpoint: 768,
+            breakpoint: 767,
             settings: {
                 slidesToShow: 3,
                 slidesToScroll: 1
@@ -55,4 +55,89 @@ $('.cert-nav.prev').click(function(){
 });
 $('.cert-nav.next').click(function(){
     $('.cert ul').slick('slickNext');
+});
+
+
+// Map
+
+ymaps.ready(init);
+
+var myMap,
+    myPlacemark;
+
+function init(){
+    myMap = new ymaps.Map("map", {
+        center: [53.9588,27.7370],
+        zoom: 14,
+        controls: ['smallMapDefaultSet']
+    });
+
+    myPlacemark = new ymaps.Placemark([53.9588,27.7370], {
+        hintContent: ''
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'assets/template/img/placemark.png',
+        iconImageSize: [36, 52],
+        iconImageOffset: [-18, -52]
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+    myMap.behaviors.disable('multiTouch');
+    myMap.geoObjects.add(myPlacemark);
+}
+
+
+$(".btn-modal").fancybox({
+    'padding' : 0
+});
+
+
+
+$('.btn.by').click(function(){
+    $('.product-hide.by').slideToggle('fast');
+});
+
+$('.btn.rus').click(function(){
+    $('.product-hide.rus').slideToggle('fast');
+});
+
+
+$(document).ready(function() {
+
+    $('.btn-submit').click(function() {
+
+        $('body').find('form:not(this)').children('label').removeClass('red'); //удаление всех сообщение об ошибке(валидатора)
+        var answer = checkForm($(this).parent().get(0)); //ответ от валидатора
+        if(answer != false)
+        {
+            var $form   = $(this).parent(),
+                name    =    $('input[name="name"]', $form).val(),
+                phone   =    $('input[name="phone"]', $form).val();
+                message =    $('textarea[name="message"]', $form).val();
+            console.log(name, phone,message);
+            $.ajax({
+                type: "POST",
+                url: "form-handler.php",
+                data: {name: name, phone: phone, message: message}
+            }).done(function(msg) {
+                console.log(name, phone);
+                $('form').find('input[type=text], textarea').val('');
+                console.log('удачно');
+                $.fancybox(
+                    '<div class="modal-box">'+ '<span class="modal-box-title">Спасибо, Ваша заявка принята!</span><br/>В скором времени с вами свяжутся наши менеджеры' +'</div>',
+                    {
+                        'autoDimensions'  : false,
+                        'padding': 0,
+                        'minWidth': 600,
+                        'transitionIn'    : 'none',
+                        'transitionOut'   : 'none'
+                    }
+                );
+                setTimeout("$.fancybox.close()", 3000);
+            });
+        }
+
+        var h = $(window).height();
+    });
+
 });
