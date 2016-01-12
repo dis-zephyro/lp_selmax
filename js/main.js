1891d2bd3f3b5faebeec299f2cab61471cd5bb17
dis-zephyro <zephyro@yandex.ru>
2016-01-13 01:09:15 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -91,6 +91,10 @@ $(".btn-modal").fancybox({
     'padding' : 0
 });
 
+// ----- Маска ----------
+jQuery(function($){
+    $("input[name='phone']").mask("+7(999) 999-9999");
+});
 
 
 $('.btn.by').click(function(){
@@ -137,7 +141,36 @@ $(document).ready(function() {
             });
         }
 
-        var h = $(window).height();
     });
+
+
+    // Анимация
+    var Android = navigator.userAgent.search(/Android/i);
+    var iPhone = navigator.userAgent.search(/iPhone/i);
+    var iPad = navigator.userAgent.search(/iPad/i);
+    if(Android != -1 || iPhone != -1 || iPad != -1) {
+
+    } else {
+
+
+        $(".scroll").each(function () { // анимация по скролу(используйте этот) достаточно добавить анимируемому блоку класс 'scroll' а css анимацию писать так: '.animated.класс_блока'
+            var block = $(this);
+            $(window).scroll(function() {
+                var top = block.offset().top;
+                var bottom = block.height()+top;
+                top = top - $(window).height();
+                var scroll_top = $(this).scrollTop();
+                if ((scroll_top > top) && (scroll_top < bottom)) {
+                    if (!block.hasClass("animated")) {
+                        block.addClass("animated");
+                    }
+                } else {
+                    block.removeClass("animated");
+                }
+            });
+        });
+        $('head').append('<link rel="stylesheet" type="text/css" href="css/animate.css" />'); //подключение файла animation.css если не мобильник
+        $('head').append('<link rel="stylesheet" type="text/css" href="css/animation.css" />'); //подключение файла animation.css если не мобильник
+    }
 
 });
